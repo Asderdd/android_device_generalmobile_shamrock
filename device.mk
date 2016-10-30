a3c7473af196be792b4680f6b1de09664c6f6e44
Asderdd <aliihsan.hatun@turkdevs.com>
2016-10-31 02:44:33 +0300

    shamrock: healthd: Add libhealthd extension library

--- device.mk
+++ device.mk
@@ -99,6 +99,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Gello
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
