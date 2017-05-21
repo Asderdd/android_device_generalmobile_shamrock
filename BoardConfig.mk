4488588e561791a9a52c80a9d06bc94d373091a9
Asderdd <aliihsan.hatun@turkdevs.com>
2017-05-21 20:06:17 +0000

    shamrock: Use Snapdragon LLVM toolchain
    Change-Id: I22dcebb474af18718a70bd8567d60051b721bd37

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,6 +173,7 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QC_TIME_SERVICES := true
+TARGET_USE_SDCLANG := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
